701c1ccc40bee5c54c18499409ba0bdbf8b8e908
UlisesPlaRocher <ulisesplarocher@gmail.com>
2024-06-17 23:40:04 +0200

    Initial commit

---
+++ favourite_car.py
@@ -0,0 +1,30 @@
+
+
+
+from car import Car
+
+
+
+
+my_new_car = Car('Alfa RomeO', '147', 2001)
+print(my_new_car.get_descriptive_name())
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
