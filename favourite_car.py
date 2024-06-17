e5a319f44bd9a8a264b7a7831edb8ddfea7ae247
UlisesPlaRocher <ulisesplarocher@gmail.com>
2024-06-18 00:19:52 +0200

    aver2

--- favourite_car.py
+++ favourite_car.py
@@ -6,7 +6,7 @@ from car import Car
 
 
 
-my_new_car = Car('AlfaRom', '147776', 1957)
+my_new_car = Car('AlfaBetaRom', '147776', 1957)
 print(my_new_car.get_descriptive_name())
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
