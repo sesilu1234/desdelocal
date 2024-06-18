fc00979984cca4c7406e35ae6dbc491b41ed3a52
UlisesPlaRocher <ulisesplarocher@gmail.com>
2024-06-18 23:16:28 +0200

    maere2

--- favourite_car.py
+++ favourite_car.py
@@ -6,11 +6,13 @@ from car import Car
 
 
 
+
 my_new_car = Car('Chrysleradsdkkkkkkas', 'Plymouth', 1957)
 
 
 
 print("hOLA")
+
 
 print(my_new_car.get_descriptive_name())
 my_new_car.odometer_reading = 50
